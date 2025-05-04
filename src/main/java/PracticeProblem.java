02a9c1aeb53ac77fea070599bcf7ce142ec2fa5f
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-05-04 00:49:07 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,20 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		// coonvert to string
+		System.out.println(CampbellClass.convertString(3));
+		System.out.println(CampbellClass.convertString(3.14));
+		System.out.println(CampbellClass.convertString(']'));
+		System.out.println(CampbellClass.convertString(false));
 
+		//convert from m to cm
+		System.out.println(CampbellClass.convertMetersToCenti(34));
+		System.out.println(CampbellClass.convertMetersToCenti(12));
+
+		//remove non alpha
+		System.out.println(CampbellClass.removeNonAlpha("Care Givers"));
+		System.out.println(CampbellClass.removeNonAlpha("What-ever"));
+		
 	}
 
 }
